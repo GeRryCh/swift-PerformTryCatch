f1662304d724432898b659a33b0fda3af49594d3
German Velibekov <german.velibekov@gmail.com>
2020-04-05 15:48:23 +0300

    providing examples

--- Usage.playground/Contents.swift
+++ Usage.playground/Contents.swift
@@ -1,8 +1,9 @@
 import UIKit
+import EventKit
 
-/* Examples for
+/* Example for
  public func perform<T>(_ expression: @autoclosure () throws -> T,
- onThrow errorExpression: @autoclosure () -> Error) throws -> T`
+                        onThrow errorExpression: @autoclosure () -> Error) throws -> T`
  */
 
 enum SearchError: Error {
@@ -20,4 +21,50 @@ func loadSearchData(matching query: String) throws -> Data {
                        orThrow: SearchError.dataLoadingFailed(url))
 }
 
+/* Example for
+public func perform<T>(_ expression: @autoclosure () throws -> T,
+                       errorTransform: (Error) -> Error) throws -> T
+*/
+enum UserError: Error {
+    case dataLoadingFailed(URL, Error)
+}
 
+func loadUserData(id: String) throws -> Data {
+    let urlString = "https://my.api.com/user?id=\(id)"
+    
+    let url = URL(string: urlString)!
+    
+    return try perform(Data(contentsOf: url)) {
+        UserError.dataLoadingFailed(url, $0)
+    }
+}
+
+/* Example for
+public func perform(_ expression: @autoclosure () throws -> (),
+                    errorCallback: (Error) -> ())
+*/
+enum CalendarEventError: Error {
+    case unauthorized
+    case accessDenied
+}
+
+func insertEventToDefaultCalendar(event :EKEvent) throws {
+    //...
+    switch EKEventStore.authorizationStatus(for: .event) {
+    case .authorized:
+        //insert event
+        break
+    case .denied:
+        throw CalendarEventError.accessDenied
+
+    //...
+    //Taken as part of the example from: https://stackoverflow.com/questions/33213715/swift-throw-from-closure-nested-in-a-function
+    default:
+        break
+    }
+}
+
+perform(try insertEventToDefaultCalendar(event: EKEvent())) {
+    guard let error = $0 as? CalendarEventError else { return }
+    print(error)
+}
--- Usage.playground/Sources/PerformTryCatch.swift
+++ Usage.playground/Sources/PerformTryCatch.swift
@@ -16,6 +16,10 @@ public func perform<T>(_ expression: @autoclosure () throws -> T,
     }
 }
 
+/// Use this method for manipulating the original error before throw
+/// - Parameters:
+///   - expression: Throwing expression
+///   - errorTransform: The closure to manipulate a thrown error. Returns an error that is eventually thrown.
 public func perform<T>(_ expression: @autoclosure () throws -> T,
                        errorTransform: (Error) -> Error) throws -> T {
     do {
@@ -25,19 +29,16 @@ public func perform<T>(_ expression: @autoclosure () throws -> T,
     }
 }
 
+/// Use this method to provide for each throwing expressions what should be done if an error is thrown.
+/// In contrast, this method doesn't throw, but used as a wrapper around `do-try-catch` dance.
+/// - Parameters:
+///   - expression: Throwing expression
+///   - errorCallback: The closure is being called when error is thrown.
 public func perform(_ expression: @autoclosure () throws -> (),
-                    orThrow errorCallback: (Error) -> ()) {
+                    errorCallback: (Error) -> ()) {
     do {
         try expression()
     } catch {
         errorCallback(error)
-    }
-}
-
-public func perform<T>(_ expression: @autoclosure () throws -> T) -> T? {
-    do {
-        return try expression()
-    } catch {
-        return nil
     }
 }
